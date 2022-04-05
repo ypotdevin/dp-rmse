ad2ca26775c5ee4ab33e6a290d1c12e564073da9
Yannik Potdevin <ypo@informatik.uni-kiel.de>
2022-04-05 07:25:21 +0000

    Added dp-rmse implementation and benchmarks.

---
+++ benchmark_rmse.py
@@ -0,0 +1,83 @@
+from typing import Iterable, Sequence
+
+import numpy as np
+import pandas as pd
+
+import dp_rmse
+
+
+def params_generator(rng: np.random.Generator):
+    sample_sizes = [1, 10, 100, 1000, 10000]
+    locations = [1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6]
+
+    for loc in locations:
+        scales = [1e-3 * loc, 1e-2 * loc, 1e-1 * loc, 1e0 * loc, 1e1 * loc]
+        for scale in scales:
+            for n in sample_sizes:
+                yield dict(
+                    size=n, loc=loc, scale=scale,
+                )
+    for n in sample_sizes:
+        yield dict(
+            size=n, loc=0.0, scale=1.0,
+        )
+
+
+def compare_on(
+    sample: np.ndarray, epsilons: Iterable[float], U: float, rng: np.random.Generator,
+) -> Sequence[dict[str, float]]:
+    leaky_rmse = np.sqrt(sample.mean())
+    sorted_sample = np.sort(sample)
+    results = [
+        dict(
+            leaky_rmse=leaky_rmse,
+            dp_rmse=dp_rmse.dp_rms_cauchy(sorted_sample, eps, U, rng),
+            execution=i,
+            epsilon=eps,
+        )
+        for i in range(10)
+        for eps in epsilons
+    ]
+    return results
+
+
+def _loc_scale(loc_scale_dist_fun, name):
+    epsilons = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0]
+    rng = np.random.default_rng()
+    lines = []
+    for params in params_generator(rng):
+        sample = loc_scale_dist_fun(rng, params["loc"], params["scale"], params["size"])
+        sample = np.abs(sample)
+        U = sample.max()
+        comparisons = compare_on(sample, epsilons, U, rng)
+        for comparison in comparisons:
+            line = dict(**params, **comparison, U=U, distribution=name)
+            print(line)
+            lines.append(line)
+    df = pd.DataFrame(lines)
+    df.to_csv("comparison_{}.csv".format(name))
+
+
+def laplace():
+    f = lambda rng, loc, scale, size: rng.laplace(loc, scale, size)
+    _loc_scale(f, "laplace")
+
+
+def normal():
+    f = lambda rng, loc, scale, size: rng.normal(loc, scale, size)
+    _loc_scale(f, "normal")
+
+
+def poisson():
+    f = lambda rng, loc, _, size: rng.poisson(loc, size)
+    _loc_scale(f, "poisson")
+
+
+def main():
+    laplace()
+    normal()
+    poisson()
+
+
+if __name__ == "__main__":
+    main()
--- dp_rmse.py
+++ dp_rmse.py
@@ -1,1 +1,70 @@
+import numpy as np
 
+#/**
+# * @param errors The errors (differences) to apply the root mean squared error
+# * function on.
+# * @param epsilon The privacy budget.
+# * @param U The upper bound on the error terms in errors.
+# * @param rng The (pseudo) random number generator to use when drawing from the
+# * Cauchy distribution.
+# * @return double The epsilon-differentially private rMSE estimate of errors.
+# */
+def dp_rms_cauchy(errors: np.ndarray, epsilon: float, U: float, rng = None) -> float:
+    if rng is None:
+        rng = np.random.default_rng()
+    sorted_errors = np.sort(errors)
+    gamma = 2.0
+    beta = epsilon / (2 * (gamma + 1.0))
+    (sens, rmse) = rMS_smooth_sensitivity(sorted_errors, beta, U)
+    noise = rng.standard_cauchy()
+    dp_rmse = rmse + 2 * (gamma + 1) * sens * noise / epsilon
+    return dp_rmse
+
+#/**
+# * @param errors The precomputed errors (to avoid having two arguments which
+# * then have to be subtracted), sorted ascendingly.
+# * @param beta The beta defining the beta-smooth sensitivity.
+# * @param U The upper bound on the errors (not squared errors).
+# * @return std::tuple<double, double> The beta-smooth sensitivity and the result
+# * of the root mean squared error function, i.e. the function
+# *
+# *     e_1, ..., e_n |-> sqrt((e_1 ** 2 + ... + e_n ** 2) / n).
+# */
+def rMS_smooth_sensitivity(errors: np.ndarray, beta: float, U: float) -> tuple[float, float]:
+    # If U is chosen well, i.e. a true upper bound on the errors, the clipping
+    # will have no effect.
+    np.clip(errors, -U, U, out=errors)
+    U = U ** 2
+    sqe_sum = errors.sum()
+    n = len(errors)
+    rmse = np.sqrt(sqe_sum / n)
+
+    smooth_sens = -np.inf
+    (prefix_sum, suffix_sum) = (sqe_sum, sqe_sum)
+    for k in range(1, n + 1):
+        largest = errors[n - k]
+        smallest = errors[k - 1]
+        prefix_sum -= largest # implicitly replace largest by 0)
+        suffix_sum -= smallest
+        prefix_local_sens = _local_sensitivity(largest, 0, prefix_sum, n, U)
+        suffix_local_sens = _local_sensitivity(smallest, U, suffix_sum, n, U)
+        local_sens = max(prefix_local_sens, suffix_local_sens)
+        smooth_sens = max(smooth_sens, local_sens * np.exp(-beta * k))
+        suffix_sum += U # replace smallest by U, but only after calculation
+    return (smooth_sens, rmse)
+
+#/**
+# * @brief The local sensitivity of the rMSE function, already operating on the
+# * vector of differences (not on two vectors which then will be subtracted).
+# *
+# * @param x the current squared error to replace.
+# * @param substitute the replacement for x.
+# * @param s the sum of the squared errors, but *without* x.
+# * @param n the number of squared error (terms) in s, plus 1 for x.
+# * @param U the upper bound of the squared errors terms in s, and x.
+# * @return double The local sensitivity of the root mean squared error function.
+# */
+def _local_sensitivity(x: float, substitute: float, s: float, n: int, U: float) -> float:
+    s = max(s, 1e-12) # to avoid division by zero
+    sens = np.sqrt(s / n) * np.abs(np.sqrt(1 + x / s) - np.sqrt(1 + substitute / s))
+    return sens
