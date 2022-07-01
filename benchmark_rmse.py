5c644d6df193f675b13cb1fd9f236a6d18dd7303
Yannik Potdevin <ypo@informatik.uni-kiel.de>
2022-07-01 11:20:47 +0000

    Update benchmark

--- benchmark_rmse.py
+++ benchmark_rmse.py
@@ -1,88 +1,86 @@
-from typing import Iterable, Sequence
+from typing import Any, cast, Iterable
 
 import numpy as np
-import pandas as pd
+import pandas as pd  # type: ignore
+from joblib import Parallel, delayed  # type: ignore
 
 import dp_rmse
 
-
-def params_generator():
-    sample_sizes = [1, 10, 100, 1000, 10000]
-    locations = [1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6]
-
-    for loc in locations:
-        scales = [1e-3 * loc, 1e-2 * loc, 1e-1 * loc, 1e0 * loc, 1e1 * loc]
-        for scale in scales:
-            for n in sample_sizes:
-                yield dict(
-                    size=n, loc=loc, scale=scale,
-                )
-    for n in sample_sizes:
-        yield dict(
-            size=n, loc=0.0, scale=1.0,
-        )
+BenchmarkResults = pd.DataFrame
+HyperParameters = dict[str, Any]
+SampleParameters = dict[str, Any]
+Sample = np.ndarray
 
 
-def compare_on(
-    sample: np.ndarray, epsilons: Iterable[float], U: float, rng: np.random.Generator,
-) -> Sequence[dict[str, float]]:
-    leaky_rmse = np.sqrt(sample.mean())
-    sorted_sample = np.sort(sample)
-    results = [
-        dict(
-            leaky_rmse=leaky_rmse,
-            dp_rmse=dp_rmse.dp_rms_cauchy(sorted_sample, eps, U, rng),
-            execution=i,
-            epsilon=eps,
-        )
-        for i in range(10)
-        for eps in epsilons
-    ]
-    return results
+def params_generator() -> Iterable[SampleParameters]:
+    sample_sizes = [10, 100, 1000, 10000]
+    locations = [1e-3, 1e-1, 1e0, 1e1, 1e3, 1e6]
+
+    for variant in ["normal", "laplace"]:
+        for loc in locations:
+            scales = [1e-1 * loc, 1e0 * loc, 1e1 * loc]
+            for scale in scales:
+                for n in sample_sizes:
+                    yield dict(size=n, loc=loc, scale=scale, variant=variant)
+        for n in sample_sizes:
+            yield dict(size=n, loc=0.0, scale=1.0, variant=variant)
 
 
-def _loc_scale(loc_scale_dist_fun, name):
-    epsilons = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0]
-    rng = np.random.default_rng()
-    lines = []
-    for params in params_generator():
-        sample = loc_scale_dist_fun(rng, params["loc"], params["scale"], params["size"])
-        sample = np.abs(sample)
-        for U in [
-            np.percentile(sample, 80),
-            np.percentile(sample, 90),
-            np.percentile(sample, 95),
-            sample.max(),
-        ]:
-            comparisons = compare_on(sample, epsilons, U, rng)
-            for comparison in comparisons:
-                line = dict(**params, **comparison, U=U, distribution=name)
-                print(line)
-                lines.append(line)
-    df = pd.DataFrame(lines)
-    df.to_csv("comparison_{}.csv".format(name))
+def sample(parameters: SampleParameters, rng: np.random.Generator,) -> Sample:
+    variant_fun_dispatcher = dict(
+        normal=rng.normal, laplace=rng.laplace, poisson=rng.poisson,
+    )
+    params = parameters.copy()
+    variant = cast(str, params.pop("variant"))
+    fun = variant_fun_dispatcher[variant]
+    sample = cast(Sample, fun(**params))
+    sample = np.abs(sample)
+    sample.sort()
+    return sample
+
+def rmse(errors: Sample) -> float:
+    return np.sqrt(np.mean(errors ** 2))
+
+def _benchmark(sample: Sample, hyperparams: HyperParameters) -> BenchmarkResults:
+    """Perform a single run of the DP rmse estimation.
+
+    Args:
+        sample (Sample):
+            The sample to calculate the DP sample variance for.
+        hyperparams (HyperParameters):
+            The hyperparameters to use for estimating the sample
+            variance. Should contain the keys "epsilon" and "U".
+
+    Returns:
+       BenchmarkResults: A line of benchmark results, shipped as a data
+       frame. It contains the (true) rMSE, the DP estimate and key
+       hyperparameters.
+    """
+    _dp_rmse = dp_rmse.dp_rms_cauchy(errors=sample, **hyperparams)
+    result = dict(rmse=rmse(sample), dp_rmse=_dp_rmse, **hyperparams)
+    return pd.DataFrame(result, index=[0])
 
 
-def laplace():
-    f = lambda rng, loc, scale, size: rng.laplace(loc, scale, size)
-    _loc_scale(f, "laplace")
-
-
-def normal():
-    f = lambda rng, loc, scale, size: rng.normal(loc, scale, size)
-    _loc_scale(f, "normal")
-
-
-def poisson():
-    f = lambda rng, loc, _, size: rng.poisson(loc, size)
-    _loc_scale(f, "poisson")
-
-
-def main():
-    laplace()
-    normal()
-    poisson()
+def benchmark(sample_params: SampleParameters) -> BenchmarkResults:
+    n_repetitions = 10
+    sampl = sample(sample_params, np.random.default_rng())
+    results = []
+    for epsilon in [0.01, 0.1, 1.0, 10]:
+        for _ in range(n_repetitions):
+            hyperparams = dict(
+                epsilon=epsilon,
+                U=sampl.max(),
+            )
+            result = _benchmark(sampl, hyperparams)
+            result = result.assign(**sample_params, **hyperparams)
+            print(result)
+            results.append(result)
+    return pd.concat(results)
 
 
 if __name__ == "__main__":
-    main()
+    sub_results = Parallel(n_jobs=126)(
+        delayed(benchmark)(sample_params) for sample_params in params_generator()
+    )
+    df = pd.concat(sub_results)
+    df.to_csv("benchmark.csv", index=False)
